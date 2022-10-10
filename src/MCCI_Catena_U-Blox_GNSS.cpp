584a36551e5e5debf00619c81b7345acf34b3783
Pranau-R <pranau@mcci.com>
2022-10-10 18:50:22 +0530

    WIP: Initial Commit

---
+++ src/MCCI_Catena_U-Blox_GNSS.cpp
@@ -0,0 +1,18 @@
+/*
+
+Module: MCCI_Catena_U-Blox_GNSS.cpp
+
+Function:
+    Implementation code for MCCI Catena U-Blox GNSS library.
+
+Copyright and License:
+    See accompanying LICENSE file for copyright and license information.
+
+Author:
+    Pranau R, MCCI Corporation   October 2022
+
+*/
+
+#include "MCCI_Catena_U-Blox_GNSS.h"
+
+/**** end of MCCI_Catena_U-Blox_GNSS.cpp ****/
---
+++ src/MCCI_Catena_U-Blox_GNSS.h
@@ -0,0 +1,23 @@
+/*
+
+Module: MCCI_Catena_U-Blox_GNSS.h
+
+Function:
+    Top-level include file for MCCI Catena U-Blox GNSS library.
+
+Copyright and License:
+    See accompanying LICENSE file for copyright and license information.
+
+Author:
+    Pranau R, MCCI Corporation   October 2022
+
+*/
+
+/// \file
+
+#ifndef _MCCI_CATENA_U_BLOX_GNSS_H_
+#define _MCCI_CATENA_U_BLOX_GNSS_H_ /* prevent multiple includes */
+
+#pragma once
+
+#endif /* _MCCI_CATENA_U_BLOX_GNSS_H_ */
